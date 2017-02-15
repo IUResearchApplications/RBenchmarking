cd439ecd81e2188277e8bb7899547fc7a9623296
jrmccombs <jmccombs@iu.edu>
2017-02-15 18:32:57 -0500

    Commit of modified benchmarking_utils.R file that did not make it into the previous commit.

--- benchmarking_utils.R
+++ benchmarking_utils.R
@@ -1,16 +1,16 @@
-get_configurable_env_parameter<-function(configurable_variable) {
+get_configurable_env_parameter <- function(configurableVariable) {
    # Get the name of the wanted environment varible from the
    # configurable environment variable.
-   env_variable <- Sys.getenv(configurable_variable)
+   envVariable <- Sys.getenv(configurableVariable)
 
-   if (env_variable == "") {
-      write(sprintf("ERROR: Configurable environment variable '%s' was not set", configurable_variable), stderr())
+   if (envVariable == "") {
+      write(sprintf("ERROR: Configurable environment variable '%s' is undefined", configurableVariable), stderr())
       quit(status=1)
    } else {
-      value <- Sys.getenv(env_variable)
+      value <- Sys.getenv(envVariable)
 
       if (value == "") {
-         write(sprintf("ERROR: Environment variable '%s' was not set", env_variable), stderr())
+         write(sprintf("ERROR: Environment variable '%s' is undefined", envVariable), stderr())
          quit(status=1)
       }
    }
@@ -19,7 +19,28 @@ get_configurable_env_parameter<-function(configurable_variable) {
 }
 
 
-read_parameters<-function() {
+set_configurable_env_parameter <- function(configurableVariable, value) {
+   # Get the name of the wanted environment varible from the
+   # configurable environment variable.
+   envVariable <- Sys.getenv(configurableVariable)
+
+   if (envVariable == "") {
+      write(sprintf("ERROR: Configurable environment variable '%s' is undefined", configurableVariable), stderr())
+      quit(status=1)
+   } else {
+      args <- list(value)
+      names(args) <- envVariable
+      returnValues <- do.call(Sys.setenv, args)
+
+      if (returnValues[1] == FALSE) {
+         write(sprintf("ERROR: Environment variable '%s' was not set", envVariable), stderr())
+         quit(status=1)
+      }
+   }
+}
+
+
+read_parameters <- function() {
 
    # For SLURM:
    # R_BENCH_NUM_THREADS_VARIABLE = MKL_NUM_THREADS
@@ -33,98 +54,94 @@ read_parameters<-function() {
    # R_BENCH_JOB_DESCRIPTOR_VARIABLE = PBS_JOBNAME
    # R_BENCH_TEST_SUITE_ID_VARIABLE = PBS_JOBID
 
-   num_threads <- strtoi(get_configurable_env_parameter("R_BENCH_NUM_THREADS_VARIABLE")) 
+   numberOfThreads <- strtoi(get_configurable_env_parameter("R_BENCH_NUM_THREADS_VARIABLE")) 
 
-   if (is.na(num_threads)) {
+   if (is.na(numberOfThreads)) {
       write("ERROR: Non-integer number of threads specified in environment", stderr())
       quit(status=1)
    }
 
-   output_directory_str <- get_configurable_env_parameter("R_BENCH_OUTPUT_DIR_VARIABLE")
-   job_descriptor_str <- get_configurable_env_parameter("R_BENCH_JOB_DESCRIPTOR_VARIABLE")
+   outputDirectoryStr <- get_configurable_env_parameter("R_BENCH_OUTPUT_DIR_VARIABLE")
+   jobDescriptorStr <- get_configurable_env_parameter("R_BENCH_JOB_DESCRIPTOR_VARIABLE")
    #test_suite_job_id_str <- get_configurable_env_parameter("R_BENCH_TEST_SUITE_ID_VARIABLE")
-   csv_results_file <- paste(output_directory_str, "/", job_descriptor_str, ".csv", sep="")
+   csvResultsFile <- paste(outputDirectoryStr, "/", jobDescriptorStr, ".csv", sep="")
 
-   return (list("num_threads" = num_threads, "csv_results_file" = csv_results_file))
+   return (list("numberOfThreads" = numberOfThreads, "csvResultsFile" = csvResultsFile))
 }
   
 
  
 
-setup_runs<-function(sizes, num_runs, num_warmup_runs, csv_results_file) {
-   num_sizes <- length(sizes)
-   max_runs <- max(num_runs)
+#
+#setup_runs <- function(dimensions, numberOfTrials, numberOfWarmupTrials, csvResultsFile) {
+#   runParameters <- list("numberOfDimension", "maximumNumberOfTrials", "runif", "rnorm")
+#   runParameters$numberOfDimensions <- length(dimensions)
+#   maxNumberOfTrials <- max(numberOfTrials)
+#   
+#   if (numberOfSizes != length(numberOfTrials)) {
+#      write("ERROR: Length of numberOfTrials and dimensions arrays must be equal", stderr())
+#      quit(status=1)
+#   }
+#
+#   if (numberOfDimensions != length(numberOfWarmupTrials)) {
+#      write("ERROR: Length of numberOfWarmupTrials and dimensions arrays must be equal", stderr())
+#      quit(status=1)
+#   }
+#
+#   times <- rep(0, maximumNumberOfRuns*numberOfDimenions);
+#   averages <- rep(0, numberOfDimensions);
+#   dim(times) <- c(maximumNumberOfTrials, numberOfDimensions)
+#
+#   Runif <- runif
+#   Rnorm <- rnorm
+#
+#   if (csv_results_file != "" && !file.exists(csv_results_file)) {
+#      cat("Dimension", "Avg. Wall Clock Time", "Number of Threads\n", file=csv_results_file, sep=",", append=FALSE)
+#   }
+#
+#   return (list("num_sizes" = num_sizes, "max_runs" = max_runs, "times" = times, "averages" = averages, "Runif" = Runif, "Rnorm" = Rnorm))
+#}
+
+
+print_results <- function(benchmarkName, numberOfThreads, dimensions, numberOfSuccessfulTrials, trialTimes, averageWallClockTimes, standardDeviations) {
+   numberOfDimensions <- length(dimensions)
+
+   cat(  "---------------------------------------------------------------------------------\n")
+   cat(sprintf("Timings summary for %s\n", benchmarkName))
+   cat(  "---------------------------------------------------------------------------------\n")
+   cat(sprintf("numberOfThreads=%s\n", numberOfThreads))
+   cat(  "---------------------------------------------------------------------------------\n")
+   cat(c("Dimension | Number of | Min Wall     | Max Wall     | Average      | Standard\n"))
+   cat(c("          | Trials    | Time         | Time         | Wall Time    | deviation\n"))
+   cat(  "---------------------------------------------------------------------------------\n")
+
+   for (i in 1:numberOfDimensions) {
+      if (numberOfSuccessfulTrials[i] > 0) {
+         cat(sprintf("%-9d   %-9d   %-12.6e   %-12.6e   %-12.6e   %-12.6e\n", dimensions[i], numberOfSuccessfulTrials[i], min(trialTimes[1:numberOfSuccessfulTrials[i], i]), max(trialTimes[1:numberOfSuccessfulTrials[i], i]), averageWallClockTimes[i], standardDeviations[i]))
+      }
+   }
+
+   cat(  "---------------------------------------------------------------------------------\n")
+
+}
+
+
+print_results_csv <- function(numberOfThreads, dimension, averageWallClockTime, standardDeviation, csvResultsFile) {
+   if (csvResultsFile != "" && !file.exists(csvResultsFile)) {
+      cat("Dimension", "Avg. Wall Clock Time", "Std. Dev", "Number of Threads\n", file=csvResultsFile, sep=",", append=FALSE)
+   }
+
+   if (csvResultsFile != "") {
+      cat(sprintf("%d,%12.6e,%12.6e,%d\n", dimension, averageWallClockTime, standardDeviation, numberOfThreads), file=csvResultsFile, append=TRUE)
+   }
+}
    
-   if (num_sizes != length(num_runs)) {
-      write("ERROR: Length of num_runs and sizes arrays must be equal", stderr())
-      quit(status=1)
-   }
-
-   if (num_sizes != length(num_warmup_runs)) {
-      write("ERROR: Length of num_warmup_runs and sizes arrays must be equal", stderr())
-      quit(status=1)
-   }
-
-   times <- rep(0, max_runs*num_sizes);
-   averages <- rep(0, num_sizes);
-   dim(times) <- c(max_runs, num_sizes)
-
-   Runif <- rMWC1019       # The fast uniform number generator
-   a <- rMWC1019(10, new.start=TRUE, seed=492166)  # Init. the generator
-   Rnorm <- rziggurat      # The fast normal number generator
-   b <- rziggurat(10, new.start=TRUE)      # Init. the generator
-   remove("a", "b")
-   #options(object.size=100000000)
-
-   if (csv_results_file != "" && !file.exists(csv_results_file)) {
-      cat("Dimension", "Avg. Wall Clock Time", "Number of Threads\n", file=csv_results_file, sep=",", append=FALSE)
-   }
-
-   return (list("num_sizes" = num_sizes, "max_runs" = max_runs, "times" = times, "averages" = averages, "Runif" = Runif, "Rnorm" = Rnorm))
-}
-
-
-print_results<-function(benchmark_name, sizes, num_runs, averages) {
-   num_sizes <- length(sizes)
-   omp_num_threads <- Sys.getenv("OMP_NUM_THREADS")
-   mkl_num_threads <- Sys.getenv("MKL_NUM_THREADS")
-
-   cat(  "-----------------------------------------------------------------------\n")
-   cat(sprintf("Timings summary for %s\n", benchmark_name))
-   cat(  "-----------------------------------------------------------------------\n")
-   cat(sprintf("OMP_NUM_THREADS=%s, MKL_NUM_THREADS=%s\n", omp_num_threads, mkl_num_threads))
-   cat(  "-----------------------------------------------------------------------\n")
-   cat(c("Dimension | Number of Runs | Min Time     | Max Time     | Average Time\n"))
-   cat(  "-----------------------------------------------------------------------\n")
-
-   for (i in 1:num_sizes) {
-      cat(sprintf("%-10d  %-14d   %-12.6e   %-12.6e   %-12.6e\n", sizes[i], num_runs[i], min(times[1:num_runs[i], i]), max(times[1:num_runs[i], i]), averages[i]))
-   }
-
-   cat(  "-----------------------------------------------------------------------\n")
-
-}
-
-
-print_results_csv<-function(dimension, avg_wall_time, num_threads, csv_results_file) {
-   if (csv_results_file != "") {
-      cat(sprintf("%d,%12.6e,%d\n", dimension, avg_wall_time, num_threads), file=csv_results_file, append=TRUE)
-   }
-}
-   
-
-compute_average_times<-function(num_runs, times) {
-   l <- length(num_runs)
-   averages <- rep(0, l)
-
-   for (i in 1:num_sizes) {
-      averages[i] <- mean(times[1:num_runs[i], i])
-   }
-
-   return(averages)
-}
-
 
 compute_average_time<-function(num_runs, times) {
    return(mean(times[1:num_runs]))
+}
+
+
+compute_standard_deviation<-function(num_runs, times) {
+   return(sqrt(var(times[1:num_runs])))
 }
